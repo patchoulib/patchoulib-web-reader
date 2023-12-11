24889edb44d55c93132b5efa06e6b0d667421e94
haruki <meteo.haru@gmail.com>
2023-12-11 18:34:09 +0000

    add pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [
+      solid(),
+      VitePWA()
+  ],
 })
